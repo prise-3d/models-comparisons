60daea2a697bfd08e3486dd0c172960d33455cdf
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-05-06 17:06:13 +0200

    model comparisons project first version

---
+++ display/display_estimated.py
@@ -0,0 +1,77 @@
+# main imports
+import numpy as np
+import pandas as pd
+import sys, os, argparse
+
+# image processing
+from PIL import Image
+from ipfml import utils
+from ipfml.processing import transform, segmentation
+
+import matplotlib.pyplot as plt
+
+# modules and config imports
+sys.path.insert(0, '') # trick to enable import of main folder module
+
+import custom_config as cfg
+
+dataset_folder = cfg.dataset_path
+scenes_list    = cfg.scenes_names
+zones_indices  = cfg.zones_indices
+
+
+def display_thresholds_comparisons(scene, thresholds):
+    
+    colors = ['C0', 'C1', 'C2', 'C3']
+    
+    plt.figure(figsize=(25, 20))
+    plt.rc('xtick', labelsize=16)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=16)    # fontsize of the tick labels
+    
+    # display each thresholds data from file comparisons
+    for i in thresholds:
+        data = thresholds[i]
+        plt.plot(data, label=i)
+    
+    plt.xticks(zones_indices)
+    plt.title('Comparisons of estimated vs human thresholds for ' + scene, fontsize=22)
+    plt.legend(fontsize=20)
+    plt.show()
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Read and compute entropy data file")
+
+    parser.add_argument('--thresholds', type=str, help='thresholds model data to compare')
+    parser.add_argument('--scene', type=str, help='Scene index to use', choices=cfg.scenes_indices)
+
+    args = parser.parse_args()
+
+    p_data   = args.data
+    p_scene  = args.scene
+
+    scenes_list = cfg.scenes_names
+    scenes_indices = cfg.scenes_indices
+
+    scene_index = scenes_indices.index(p_scene.strip())
+    scene = scenes_list[scene_index]
+
+    estimated_thresholds = {}
+
+    # read line by line file to estimate threshold entropy stopping criteria
+    with open(p_data, 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+
+            data = line.split(';')
+
+            del data[-1]
+
+            method_name = data[0]
+            estimated_thresholds[method_name] = [int(d) for d in data[1:] ]
+            
+    display_thresholds_comparisons(scene, estimated_thresholds)
+
+if __name__== "__main__":
+    main()
